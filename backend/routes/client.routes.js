67a72d80acd3535ad3928742133d6555e95fd95b
masterok228 <merkureva9966@gmail.com>
2020-06-04 22:06:31 +0500

    del files

--- backend/routes/client.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express-promise-router')();
-const clientController = require('../controllers/client.controller');
-
-router.get('/getClients', clientController.getClients);
-router.put('/newClient', clientController.newClient);
-router.post('/getClient', clientController.getClient);
-router.post('/editClient', clientController.editClient);
-module.exports = router;
--- backend/routes/lab4.routes.js
+++
@@ -1,5 +0,0 @@
-const router = require('express-promise-router')();
-const lab4Controller = require('../controllers/lab4.controller');
-
-router.get('/textchange', lab4Controller.changeText);
-module.exports = router;
